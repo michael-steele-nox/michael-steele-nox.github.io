8b47476f1bd9a32ea2228723730716b5de57f9bb
michael-steele-nox <48181422+michael-steele-nox@users.noreply.github.com>
2021-08-21 22:42:21 +0300

    Adde melody_studying_project folder
    Adde melody_studying_project folder

---
+++ melody_studying_project/js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+	var currentFloor = 2;
+	var counterUp = $(".counter-up");
+	var counterDown = $(".counter-down");
+  var floorPath = $(".home-image path");
+	var modal = $(".modal");
+	var modalCloseButton = $(".modal-close-button");
+	var viewFlatsButton = $(".view-flats");
+	
+    // console.log("Сайт готов к манипуляции");
+    floorPath.on("mouseover", function () {
+//      console.log("Кликнули на этаж");
+//			console.log($(this).attr("data-floor"));
+      floorPath.removeClass("current-floor");
+			currentFloor = $(this).attr("data-floor");-
+			$(".counter").text(currentFloor);
+    });
+	
+	floorPath.on('click', toggleModal);
+	modalCloseButton.on('click', toggleModal);
+	viewFlatsButton.on('click', toggleModal);
+	
+	
+	counterUp.on("click", function () {
+//		console.log("click up")
+		if (currentFloor < 18) {
+      currentFloor++;
+		  usCurrentFloor = currentFloor.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false });
+		  $(".counter").text(usCurrentFloor);
+      floorPath.removeClass("current-floor");
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+    }
+  });
+
+  counterDown.on("click", function () {
+    if (currentFloor > 2) {
+      currentFloor--;
+      usCurrentFloor = currentFloor.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false });
+      $(".counter").text(usCurrentFloor);
+      floorPath.removeClass("current-floor");
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+    }
+  });
+
+	function toggleModal() {
+		modal.toggleClass("is-open");
+	};
+	
+	
+});
